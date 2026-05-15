3f6f541af7444db30b06413f7a3f7bce87ee1f29
Karthisgowda <192409624+Karthisgowda@users.noreply.github.com>
2026-05-16 00:55:58 +0530

    Simplify React entry point

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ContextProvider } from "./context/socketContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 reportWebVitals();
